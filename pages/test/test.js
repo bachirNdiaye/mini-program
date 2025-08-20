dde64aec1aa960f1efcd16775f42fcea38f3eccb
KaboreIssouf <101973847+KaboreIssouf@users.noreply.github.com>
2025-08-20 14:59:54 +0000

    mes tests

---
+++ pages/test/test.js
@@ -0,0 +1,29 @@
+Page({
+  data: {
+     categories: ["All", "Politic", "Sport", "Education", "Gaming"],
+    activeCategory: "All"
+  },
+
+  /**
+   * Page initialization hook
+   * @returns {void}
+   */
+  onLoad() {
+  },
+
+
+  onSearch(e) {
+    console.log("Texte recherché :", e.detail);
+  },
+  onFilterClick() {
+    console.log("Bouton filtre cliqué !");
+  },
+
+ 
+
+  onBadgeClick(e) {
+    const selected = e.detail.name;
+    this.setData({ activeCategory: selected });
+    console.log("Catégorie sélectionnée :", selected);
+  }
+});
