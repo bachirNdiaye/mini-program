a9379c829b96b067572d1b13d5e76529ac7d75bf
Memories25 <enimalpoid@gmail.com>
2025-08-20 12:41:34 +0000

    creation de custom swiper

---
+++ components/custom-swiper/index.js
@@ -0,0 +1,24 @@
+Component({
+  data: {
+    // Swiper Breaking News
+    news: [
+      {
+        id: 1,
+        title: "Alexander wears modified helmet in road races",
+        category: "Sports",
+        image: "https://source.unsplash.com/600x400/?cycling"
+      },
+      {
+        id: 2,
+        title: "What Training Do Volleyball Players Need?",
+        category: "Sports",
+        image: "https://source.unsplash.com/600x400/?volleyball"
+      },
+      {
+        id: 3,
+        title: "Secondary school places: When do parents find out?",
+        category: "Education",
+        image: "https://source.unsplash.com/600x400/?school"
+      }
+    ] 
+} } )
